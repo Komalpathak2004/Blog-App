a1ab9729d444cb4576d078ec03513fb212ab495f
Komal Pathak <164751240+Komalpathak2004@users.noreply.github.com>
2024-10-23 23:34:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def get_comments(blog_id):
     return jsonify({"comments": blog['comments']}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
